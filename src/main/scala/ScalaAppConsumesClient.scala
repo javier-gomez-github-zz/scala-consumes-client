72f21d3491f21aeeeb2e61f36464f6ed49232a5e
Javier Gomez <javier.gomez@hootsuite.com>
2016-01-29 09:52:30 -0300

    Added several new methods (still in progress)

--- src/main/scala/ScalaAppConsumesClient.scala
+++ src/main/scala/ScalaAppConsumesClient.scala
@@ -78,7 +78,7 @@ object ScalaAppConsumesClient extends App {
     * Get Account Groups By Account Number
     */
 //  println("Get Account Groups By Account Number: "
-//    + client.getAccountGroupsByAcctNo(21752452L))
+//    + client.getAccountGroups(21752452L))
 
   /**
     * Get Account Groups By User Id
@@ -90,7 +90,7 @@ object ScalaAppConsumesClient extends App {
     * Set Session Id By Account Number
     */
 //  println("Set Session Id By Account Number: "
-//    + client.setSessionByAcctNo(21752452L))
+//    + client.setSession(21752452L))
 
   /**
     * Set Session Id By User Id
@@ -111,6 +111,52 @@ object ScalaAppConsumesClient extends App {
 //  println("Update Master Plan: "
 //    + client.updateMasterPlan(21752452L, options, Map()))
 
+
+  /**
+    * Assign Supplemental Plan to Account
+    */
+//  val options = Map("supp_plan_no" -> 10122070L)
+//  println("Assign Supplemental Plan to Account: "
+//    + client.assignSupplementalPlan(22533882L, options, Map()))
+
+  /**
+    * Modify Supplemental Plan of Account
+    */
+//  val options = Map("supp_plan_no" -> 10122070L)
+//  val params = Map("num_plan_units" -> "2")
+//  println("Modify Supplemental Plan of Account: "
+//    + client.modifySupplementalPlan(22533882L, options, params))
+
+  /**
+    * Modify Supplemental Plan Units of Account
+    */
+//  val options = Map("supp_plan_no" -> 10122070L)
+//  println("Modify Supplemental Plan Units of Account: "
+//    + client.modifySupplementalPlanUnits(22533882L, options, 3L))
+
+  /**
+    * Modify Supplemental Plan Rate Schedule of Account
+    */
+//  val options = Map("supp_plan_no" -> 10122070L,
+//  "alt_rate_schedule_no" -> 2L)
+//  println("Modify Supplemental Plan Rate Schedule of Account: "
+//    + client.modifySupplementalPlanRateSchedule(22533882L, options))
+
+  /**
+    * Replace Supplemental Plan of Account
+    */
+//  val options = Map("existing_supp_plan_no" -> 10122070L,
+//    "new_supp_plan_no" -> 10122068L)
+//  println("Replace Supplemental Plan of Account: "
+//    + client.replaceSupplementalPlan(22533826L, options, Map()))
+
+  /**
+    * Cancel Supplemental Plan of Account
+    */
+//  val options = Map("supp_plan_no" -> 10122070L)
+//  println("Cancel Supplemental Plan of Account: "
+//    + client.cancelSupplementalPlan(22533826L, options, Map()))
+
   /**
     * Update Account Status
     */
@@ -127,12 +173,110 @@ object ScalaAppConsumesClient extends App {
   /**
     * Update Account
     */
-//    val params = Map("first_name" -> "Javier", "last_name" -> "Gomez")
-//    println("Update Account: " + client.updateAccount(22533826L, params))
+//  val params = Map("first_name" -> "Javier", "last_name" -> "Gomez")
+//  println("Update Account: " + client.updateAccount(22533826L, params))
 
   /**
     * Update Account Contact
     */
-//    val params = Map("company_name" -> "Hootsuite Inc.")
-//    println("Update Account Contact: " + client.updateAccountContact(22533826L, params))
+//  val params = Map("company_name" -> "Hootsuite Inc.")
+//  println("Update Account Contact: " + client.updateAccountContact(22533826L, params))
+
+  /**
+    * Update Account Payment Method
+    */
+//  val params = Map("pay_method" -> "6")
+//  println("Update Account Payment Method: " + client.updateAccountPaymentMethod(22533826L, params))
+
+  /**
+    * Apply Coupon to Account
+    */
+//  println("Apply Coupon to Account: " + client.applyCouponToAccount(22533826L, "2mnthprohsu50percent", Map()))
+
+  /**
+    * Update Account Supplemental Field
+    */
+//  println("Update Account Supplemental Field: " + client.updateAccountSupplementalField(22533826L, "enterprise_affiliate_code", "1234"))
+
+  /**
+    * Create Service Credit
+    */
+//  println("Create Service Credit: " + client.createServiceCredit(22533826L, 10, 1L, Map()))
+
+  /**
+    * Create Percentage Service Credit
+    */
+//  println("Create Percentage Service Credit: " + client.createPercentageServiceCredit(22533826L, 10122070L, 100, Map()))
+
+  /**
+    * Apply Cash Credit
+    */
+//  println("Apply Cash Credit: " + client.applyCashCredit(22533826L, 10, 22L, Map()))
+
+  /**
+    * Apply Service Credit
+    */
+//  println("Apply Service Credit: " + client.applyServiceCredit(22533826L, 10, 22L, Map()))
+
+  /**
+    * Transfer Account Balance
+    */
+//  println("Transfer Account Balance: " + client.transferAccountBalance(22533826L, 22533882L, Map()))
+
+  /**
+    * Get Account Number For User
+    */
+//  println("Get Account Number For User: " + client.getAccountNumberForUser("1.trunkd2147559698"))
+
+  /**
+    * Get Transaction History
+    */
+//  println("Get Transaction History: " + client.getTransactionHistory(22533826L, Map()))
+
+  /**
+    * Get Transaction History by Date
+    */
+//  println("Get Transaction History by Date: " + client.getTransactionHistoryByDate(19132946L, "", "", -1L, 1L))
+
+  /**
+    * Void Transaction for Account
+    */
+//  println("Void Transaction for Account: " + client.voidTransaction(22533826L, "130909298", 8L, "Voidedjgomez", Map()))
+
+  /**
+    * Get Invoice History
+    */
+//  val options = Map("acct_no" -> 22533826L)
+//  println("Get Invoice History: " + client.getInvoiceHistory(options, Map()))
+
+  /**
+    * Get Invoice History By User Id
+    */
+//    println("Get Invoice History By User Id: " + client.getInvoiceHistoryByUserId("jgomez", Map()))
+
+  /**
+    * Generate Statement
+    */
+//  println("Generate Statement: " + client.generateStatement(19132946L))
+
+  /**
+    * Get Statement For Invoice
+    */
+//  val options = Map("acct_no" -> 19132946L)
+//  println("Get Statement For Invoice: " + client.getStatementForInvoice(74321595L, options, Map()))
+
+  /**
+    * Get Invoice Details
+    */
+//  println("Get Invoice Details: " + client.getInvoiceDetails(19132946L, 74321595L))
+
+  /**
+    * Get Pending Invoice Number
+    */
+//  println("Get Pending Invoice Number: " + client.getPendingInvoiceNumber(19132946L))
+
+  /**
+    * Collect from Account
+    */
+//  println("Collect from Account: " + client.collectFromAccount(19132946L, 100, Map()))
 }
